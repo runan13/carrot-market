fb1f7e510aa9dd3f8e15333197507cba93d23c5b
Kang Seokjin <tjrrh123@gmail.com>
2022-01-16 00:09:41 +0900

    Practice Tailwind CSS

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,8 +21,64 @@ const Home: NextPage = () => {
           Checkout
         </div>
       </div>
-      <div className="bg-white p-10 rounded-2xl shadow-lg"></div>
-      <div className="bg-white p-10 rounded-2xl shadow-lg"></div>
+      <div className="bg-white overflow-hidden rounded-2xl shadow-lg">
+        <div className="bg-blue-500 p-6 pb-14">
+          <span className="text-white text-xl">Profile</span>
+        </div>
+        <div className="rounded-3xl p-6 relative -top-5 bg-white">
+          <div className="flex justify-between">
+            <div className="flex flex-col items-center">
+              <span className="text-sm text-gray-500">Order</span>
+              <span className="font-medium">230</span>
+            </div>
+            <div className="h-24 w-24 bg-red-400 rounded-full items-end relative -top-20" />
+            <div className="flex flex-col items-center">
+              <span className="text-sm text-gray-500">Spent</span>
+              <span className="font-medium">$340</span>
+            </div>
+          </div>
+          <div className="realtive flex flex-col items-center -top05">
+            <span>Tony Molly</span>
+            <span>미국</span>
+          </div>
+        </div>
+      </div>
+      <div className="bg-white p-6 rounded-3xl shadow-lg">
+        <div className="flex mb-5 justify-between items-center">
+          <span>⃪</span>
+          <div className="space-x-3">
+            <span>★4.9</span>
+            <span className="shadow-xl p-2 rounded-md">♥</span>
+          </div>
+        </div>
+        <div className="bg-zinc-400 h-80 mb-5" />
+        <div className="flex flex-col">
+          <span className="font-medium text-xl">Swoon Lounge</span>
+          <span className="text-xs text-gray-500">Chair</span>
+          <div className="mt-3 mb-3 flex justify-between items-center">
+            <div>
+              <input type="radio" name="" id="" />
+              <input type="radio" name="" id="" />
+              <input type="radio" name="" id="" />
+            </div>
+            <div className="flex items-center space-x-4">
+              <button className="p-2.5 rounded-lg bg-blue-200 flex justify-center items-center aspect-square w-8 h-8 font-medium text-xl text-gray-500">
+                -
+              </button>
+              <span>1</span>
+              <button className="p-2.5 rounded-lg bg-blue-200 flex justify-center items-center aspect-square w-8 h-8 font-medium text-xl text-gray-500">
+                +
+              </button>
+            </div>
+          </div>
+          <div className="flex justify-between items-center ">
+            <span className="font-medium text-2xl">$450</span>
+            <button className="bg-blue-500 py-2 px-8 text-xs text-center text-white rounded-xl">
+              Add to cart
+            </button>
+          </div>
+        </div>
+      </div>
       <div className="bg-white p-10 rounded-2xl shadow-lg"></div>
     </div>
   );
