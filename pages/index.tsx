f89d89200286a64f517aa8e024a17568d3c30997
runan13 <tjrrhj123@gmail.com>
2022-08-07 13:45:32 +0900

    Tailwind CSS practice #2

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,7 +21,28 @@ const Home: NextPage = () => {
           Checkout
         </div>
       </div>
-      <div className="max-w-xs bg-white p-10 rounded-2xl shadow-xl"></div>
+      <div className="max-w-xs bg-white rounded-2xl shadow-xl overflow-hidden">
+        <div className="bg-blue-500 p-7 pb-14">
+          <span className="text-white text-2xl">Profile</span>
+        </div>
+        <div className="rounded-2xl relative -top-5 p-7 bg-white">
+          <div className="flex justify-between items-end relative -top-16">
+            <div className="flex flex-col items-center">
+              <span className="text-sm text-gray-500">Orders</span>
+              <span className="font-medium">340</span>
+            </div>
+            <div className="h-24 w-24 bg-red-400 rounded-full" />
+            <div className="flex flex-col items-center">
+              <span className="text-sm text-gray-500">Spent</span>
+              <span className="font-medium">$2,310</span>
+            </div>
+          </div>
+          <div className="flex flex-col items-center -mt-10 -mb-5">
+            <span className="text-lg font-medium">Tony Molloy</span>
+            <span className="text-sm text-gray-500">New York, USA</span>
+          </div>
+        </div>
+      </div>
       <div className="max-w-xs bg-white p-10 rounded-2xl shadow-xl"></div>
       <div className="max-w-xs bg-white p-10 rounded-2xl shadow-xl"></div>
     </div>
