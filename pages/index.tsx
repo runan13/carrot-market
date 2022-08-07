17688e2afaae68ee798129b9984280fd7c9bb6d7
runan13 <tjrrhj123@gmail.com>
2022-08-07 13:25:36 +0900

    Tailwind CSS practice #1

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,29 @@
 import type { NextPage } from 'next';
-import Head from 'next/head';
-import Image from 'next/image';
-import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
   return (
-    <div className="bg-red-500">
-      <h1 className="text-black">tailwind css is work!!</h1>
+    <div className="bg-slate-400 py-20 px-10 grid gap-10">
+      <div className="max-w-xs bg-white p-7 rounded-2xl shadow-xl">
+        <span className="font-semibold text-2xl">Select Item</span>
+        <div className="flex justify-between my-2">
+          <span className="text-gray-500">Grey Chair</span>
+          <span className="font-semibold">$10</span>
+        </div>
+        <div className="flex justify-between">
+          <span className="text-gray-500">Tooly Table</span>
+          <span className="font-semibold">$80</span>
+        </div>
+        <div className="flex justify-between mt-2 pt-2 border-t-2 border-dashed">
+          <span>Total</span>
+          <span className="font-semibold">$90</span>
+        </div>
+        <div className="mt-5 bg-blue-500 text-white text-center p-2 rounded-xl w-3/4 mx-auto">
+          Checkout
+        </div>
+      </div>
+      <div className="max-w-xs bg-white p-10 rounded-2xl shadow-xl"></div>
+      <div className="max-w-xs bg-white p-10 rounded-2xl shadow-xl"></div>
+      <div className="max-w-xs bg-white p-10 rounded-2xl shadow-xl"></div>
     </div>
   );
 };
