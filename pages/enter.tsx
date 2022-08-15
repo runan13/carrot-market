bf5cbbec622c5341f5dde9b19422401f2be2a90c
runan13 <tjrrhj123@gmail.com>
2022-08-15 11:13:57 +0900

    [Auth] Auth CSS #1

--- pages/enter.tsx
+++ pages/enter.tsx
@@ -1,18 +1,42 @@
 import { useState } from 'react';
 
+function cls(...className: string[]) {
+  return className.join(' ');
+}
+
 export default function Enter() {
   const [method, setMethod] = useState<'email' | 'phone'>('email');
   const onEmailClick = () => setMethod('email');
   const onPhoneClick = () => setMethod('phone');
   return (
-    <div>
-      <h3>Enter to Carrot</h3>
-      <div>
-        <div>
-          <h5>Enter using:</h5>
-          <div>
-            <button onClick={onEmailClick}>Email</button>
-            <button onClick={onPhoneClick}>Phone</button>
+    <div className="mt-16">
+      <h3 className="text-3xl font-bold text-center">Enter to Carrot</h3>
+      <div className="mt-8">
+        <div className="flex flex-col items-center">
+          <h5 className="text-sm text-gray-500 font-medium">Enter using:</h5>
+          <div className="mt-8 border-b w-full grid grid-cols-2 gap-16">
+            <button
+              className={cls(
+                `pb-4 font-medium border-b-2`,
+                method === 'email'
+                  ? 'border-orange-500 text-orange-500'
+                  : 'text-gray-500 border-transparent',
+              )}
+              onClick={onEmailClick}
+            >
+              Email
+            </button>
+            <button
+              className={cls(
+                `pb-4 font-medium border-b-2`,
+                method === 'phone'
+                  ? 'border-orange-500 text-orange-500'
+                  : 'text-gray-500 border-transparent',
+              )}
+              onClick={onPhoneClick}
+            >
+              Phone
+            </button>
           </div>
         </div>
         <form>
